564c7bf3349a488ed8a5df5f56b0f24f5cfe0443
Chris <cristianortizventas@gmail.com>
2025-11-15 20:10:20 -0500

    SErver

--- server/server.js
+++ server/server.js
@@ -57,4 +57,6 @@ app.use('/api/user', userRouter);
 app.use('/api/owner', ownerRouter);
 app.use('/api/bookings', bookingRouter);
 
-export default app;
+import { createServer } from '@vercel/node';
+
+export default createServer(app);
